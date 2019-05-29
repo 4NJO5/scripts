b16a4ab3a024dc8d279ee8afbdd2c253a9f8cdef
Anderson Anjos <andersonlosangeles@hotmail.com>
2019-05-29 19:23:40 -0300

    Atualização 20190529.1923

--- anderson-anjos/curriculum.js
+++ anderson-anjos/curriculum.js
@@ -1,1 +1,11 @@
 console.log("Seja, Bem Vindo!");
+
+jQuery(document).ready(function() {
+    $(window).scroll(function () {
+        set = $(document).scrollTop()+"px";
+        jQuery('#float-banner').animate(
+            {top:set},
+            {duration:1000, queue:false}
+        );
+    });
+});
