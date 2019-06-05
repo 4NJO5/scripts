60ae215c45a33fe0c1ff008e19620cff918e9838
Anderson Anjos <andersonlosangeles@hotmail.com>
2019-06-05 08:14:10 -0300

    Atualização 20190605.0814

--- anderson-anjos/curriculum.js
+++ anderson-anjos/curriculum.js
@@ -1,11 +1,1 @@
 console.log("Seja, Bem Vindo!");
-
-jQuery(document).ready(function() {
-    $(window).scroll(function () {
-        set = $(document).scrollTop()+"px";
-        jQuery('#float-banner').animate(
-            {top:set},
-            {duration:1000, queue:false}
-        );
-    });
-});
--- gruporojemac/renomeiaimagens/rojemac/jpgrojemac.js
+++ gruporojemac/renomeiaimagens/rojemac/jpgrojemac.js
@@ -2,6 +2,9 @@ function exibeNomeApp(){
     console.log('Renomeia Imagens para Amazon da Rojemac.');
 }
 
+function renomeiaAmazon(){
+    window.location='https://gruporojemac.dnsalias.com/candide/scripts/renomeiaJPGEANRojemacAmazoncomtraco.asp';
+}
 //setTimeout(exibeNomeApp, 1000); exibe algo e para após o time definido.
 setInterval(exibeNomeApp, 10000);
 
