80d9d99ae0f92c383129cd293d156e2af85190b7
Anderson Anjos <andersonlosangeles@hotmail.com>
2019-06-24 12:36:21 -0300

    Atualização 20190624.1236

--- alura/logicadeprogramacao/jogos/descubra-o-numero-2/javascript/don-2.js
+++ alura/logicadeprogramacao/jogos/descubra-o-numero-2/javascript/don-2.js
@@ -1,30 +1,47 @@
+//Variáveis Global do sistema
+var input = document.getElementById('chute');
+input.focus();
+
 function sair(){
-    window.location.href="./index.html"
+    window.location.href='./index.html';
+}
+function gameOver(){
+    window.location.href='./game-over.html';
 }
 
-var numeroPensado = Math.round(Math.random() * 100);
-console.log(numeroPensado);
-var audioAcertou = new Audio('/alura/logicadeprogramacao/jogos/descubra-o-numero-2/media/esta-fera.mp3');
-var audioErrou = new Audio('/alura/logicadeprogramacao/jogos/descubra-o-numero-2/media/faustao-errou.mp3');
+//Codigo de implemantação do DON2
 
-function comparar(){
+        var numeroPensado = Math.round(Math.random() * 100);
+        console.log(numeroPensado);
+        var oportunidades = 3;
+        var audioAcertou = new Audio('/alura/logicadeprogramacao/jogos/descubra-o-numero-2/media/esta-fera.mp3');
+        var audioErrou = new Audio('/alura/logicadeprogramacao/jogos/descubra-o-numero-2/media/faustao-errou.mp3');
+        var tentativas = 0;
 
-    var chute = document.getElementById('chute').value;
-        console.log(chute);
-    var tentativas = 0;
-    var oportunidades = 3;
+    function comparar(){
 
-    for( tentativas = 1; tentativas <= oportunidades; tentativas++ ){
+            var chute = input.value;
+            console.log(chute, tentativas);
 
-        if( chute == numeroPensado){
-            audioAcertou.play();
-            break;
-        }else{        
-            audioErrou.play();
-        }
-        document.getElementById('chute').value = "";
-        document.getElementById('chute').focus();
+            for(tentativas = 1; tentativas <= oportunidades; tentativas++){
 
+                if( chute == numeroPensado ){
+
+                    console.log(`Você acertou o Numero Pensado era ${numeroPensado}.`);
+                    audioAcertou.play();
+                    break;
+
+                }else{
+
+                    console.log(`Você chutou o numero ${chute} e errou na Tentativa ${tentativas}/${oportunidades}.`);
+                    audioErrou.play();
+
+                }
+            //document.getElementById('chute').value = '';
+            //document.getElementById('chute').focus();
+            }
+        setTimeout(gameOver,10000);
     }
 
-}
+
+    
--- alura/logicadeprogramacao/jogos/descubra-o-numero-2/javascript/index.js
+++ alura/logicadeprogramacao/jogos/descubra-o-numero-2/javascript/index.js
@@ -1,6 +1,7 @@
-var input = document.getElementById("nome");
-input.focus();
-
 function redirDon2(){
     window.location.href = "./don-2.html"
+}
+
+function redirGameOver(){
+    window.location.href = "./game-over.html"
 }
