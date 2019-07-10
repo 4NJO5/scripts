c530626eb0fec0cfb2dcabcab92b036dbeb4ad14
Anderson Anjos <andersonlosangeles@hotmail.com>
2019-07-10 15:32:06 -0300

    Atualização 20190710.1531

--- alura/programando-na-linguagem-da-web/introducao-javascript/js/remover-pacientes.js
+++ alura/programando-na-linguagem-da-web/introducao-javascript/js/remover-pacientes.js
@@ -1,13 +1,68 @@
-let tabela = document.querySelectorAll('table');
+const botaoExcluir = document.querySelector('#excluir-paciente');
+botaoExcluir.addEventListener('click', function(event){
+    event.preventDefault();
+    let btnAtivoExcluir = document.querySelector('.btnTabela');
+    let btnAtivoLimpar = document.querySelector('.limpaArray');
+    const deletar = window.confirm(`Deseja Exluir o Paciente Selecionado?`);
+    listaExclusao.map(function(item){
+        if(deletar == true){
+            item.classList.add('fadeOut');
+            setTimeout(function(){
+                item.remove();
+            },500)}else{
+                btnAtivoExcluir.removeAttribute('active','active');
+                btnAtivoExcluir.setAttribute('disabled','disabled');
+                btnAtivoLimpar.removeAttribute('active','active');
+                btnAtivoLimpar.setAttribute('disabled','disabled');
+                item.classList.remove('selecionaLinha');
+                item.classList.add('cancelaSelecaoLinha');
+                listaExclusao = [];
+            };
+        });
+    });
 
-tabela.addEventListener('dblclick', function(event){
-    event.target.parentNode.remove();
+tabela = document.querySelector('table');
+
+const div = document.getElementById('btnExclusao');
+
+let listaExclusao = [];
+
+mostraBtnLimpar();
+
+
+pacientes.forEach(function(paciente){
+    paciente.addEventListener('dblclick', function(){
+        let btnLimpaSelecao = document.querySelector('.limpaArray');
+        botaoExcluir.removeAttribute('disabled','disabled');
+        botaoExcluir.setAttribute('active','active');
+        btnLimpaSelecao.removeAttribute('disabled','disabled');
+        btnLimpaSelecao.setAttribute('active','active');
+        event.target.parentNode.classList.remove('cancelaSelecaoLinha');
+        event.target.parentNode.classList.add('selecionaLinha');
+        listaExclusao.push(this);
+    });
 });
 
-
-// pacientes.forEach(function(paciente){
-//     paciente.addEventListener('dblclick', function(){
-//         //console.log('Fui clicado com duplo click');
-//         this.remove();
-//     });
-// });
+function mostraBtnLimpar(){
+    let btnLimpaSelecao = document.createElement('button');
+    btnLimpaSelecao.classList.add('limpaArray')
+    btnLimpaSelecao.classList.add('btnTabela');
+    btnLimpaSelecao.classList.add('btn');
+    btnLimpaSelecao.classList.add('btn-warning');
+    btnLimpaSelecao.setAttribute('disabled','disabled');
+    btnLimpaSelecao.textContent = 'Limpar Seleção';
+    div.appendChild(btnLimpaSelecao);
+    
+    btnLimpaSelecao.addEventListener('click', function(event){
+        event.preventDefault();
+        listaExclusao.map(function(item){
+            botaoExcluir.removeAttribute('active','active');
+            botaoExcluir.setAttribute('disabled','disabled');
+            btnLimpaSelecao.removeAttribute('active','active');
+            btnLimpaSelecao.setAttribute('disabled','disabled');
+            item.classList.remove('selecionaLinha');
+            item.classList.add('cancelaSelecaoLinha');
+            listaExclusao = [];
+        });
+    });
+}
